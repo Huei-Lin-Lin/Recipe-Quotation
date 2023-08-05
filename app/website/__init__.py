700e874fb4c2ab52fa2adb3fa16a491e91a8bf86
Huei-Lin-Lin <aaaaaa121516202326@gmail.com>
2023-08-05 16:58:59 +0800

    first commit

--- app/website/__init__.py
+++ app/website/__init__.py
@@ -16,9 +16,9 @@ db_name = os.getenv('DB_NAME')
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'My flask project!'
-    # app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{user}:{passwd}@{host}:{port}/{db_name}'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://{}:{}@{}/{}?port={}?charset=utf8'.format(
-        user, passwd, host, db_name, port)
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{user}:{passwd}@{host}:{port}/{db_name}'
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://{}:{}@{}/{}?port={}?charset=utf8'.format(
+    # user, passwd, host, db_name, port)
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
