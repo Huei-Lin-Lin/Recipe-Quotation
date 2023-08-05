1c8fe721adcc31343f9914be3eb5589a159a87e0
Huei-Lin-Lin <aaaaaa121516202326@gmail.com>
2023-08-05 19:05:57 +0800

    second commit

--- app/website/db.py
+++ app/website/db.py
@@ -31,7 +31,7 @@ class MyDB:
             }
             mydb = mysql.connector.connect(**config)
             # 生成一個遊標物件 ( 相當於 cmd 開啟 mysql 中的 mysql> )
-            self.cursor = mydb.cursor()
+            self.cursor = mydb.cursor(dictionary=True)
             print("連線結果", mydb)  # 印出連線結果
         except:
             print("資料庫連接失敗：")
