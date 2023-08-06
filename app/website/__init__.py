d6c1b16618cb5acaa7f1cdf867426c18d7e8a353
Huei-Lin-Lin <aaaaaa121516202326@gmail.com>
2023-08-06 10:01:24 +0800

    third commit

--- app/website/__init__.py
+++ app/website/__init__.py
@@ -22,9 +22,9 @@ def create_app():
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     # db.init_app(app)
 
-    with app.app_context():
-        db.db = db.connect()
-        print("=====", db.db, "=====")
+    # with app.app_context():
+    #     db.db = db.connect()
+    #     print("=====", db.db, "=====")
 
     from .views import views
     app.register_blueprint(views, url_prefix='/')
